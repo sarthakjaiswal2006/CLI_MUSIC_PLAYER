2ade1a4235939c3ed85803e377197236e1987637
sarthak <sarthakj586@gmail.com>
2026-09-10 14:37:46 +0530

    imported required modules

--- player.js
+++ player.js
@@ -1,6 +1,4 @@
 const { spawn } = require("child_process");
 const fs = require("fs");
-
-// Read all the mp3 files
 const path = "./songs";
 const songs = fs.readdirSync(path).filter((el) => el.endsWith(".mp3"));

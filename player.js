3c8ee29b55c2c0c5b98c5c701cdd408242dbec3e
sarthak <sarthakj586@gmail.com>
2026-09-11 19:32:23 +0530

    ADDED RESUME AND PAUSE

--- player.js
+++ player.js
@@ -8,7 +8,7 @@ for (let i = 0; i < songs.length; i++) {
   console.log(`${i + 1}: ${songs[i].split(".")[0]}`);
 }
 console.log(`\n🎵 Select a number to play the song`);
-console.log(`q = quit\n`);
+console.log(`p = pause | r = resume | q = quit\n`);
 process.stdin.setEncoding("utf-8");
 process.stdin.setRawMode(true);
 let childProcess = null;
@@ -16,11 +16,21 @@ process.stdin.on("data", (input) => {
   const userInput = input.toString().trim();
   if (userInput === "q") {
     if (childProcess) {
-      childProcess.kill();
+      childProcess.kill("SIGCONT");
+      childProcess.kill("SIGTERM");
     }
     process.stdin.setRawMode(false);
     process.stdin.pause();
+    console.log("👋 Goodbye!");
     process.exit(0);
+  }
+  if (userInput === "p") {
+    pause();
+    return;
+  }
+  if (userInput === "r") {
+    resume();
+    return;
   }
   if (userInput > 0 && userInput <= songs.length) {
     player(+userInput);
@@ -30,7 +40,8 @@ function player(userInput) {
   console.log(`Selected Song: ${songs[userInput - 1]}`);
 
   if (childProcess) {
-    childProcess.kill();
+    childProcess.kill("SIGCONT");
+    childProcess.kill("SIGTERM");
   }
   const songPath = path.join(songsPath, songs[userInput - 1]);
   childProcess = spawn("afplay", [songPath]);
@@ -38,4 +49,16 @@ function player(userInput) {
     console.log("Song finished...");
     childProcess = null;
   });
+}
+function pause() {
+  if (childProcess) {
+    childProcess.kill("SIGSTOP");
+    console.log("⏸️ Song paused");
+  }
+}
+function resume() {
+  if (childProcess) {
+    childProcess.kill("SIGCONT");
+    console.log("▶️ Song resumed");
+  }
 }
